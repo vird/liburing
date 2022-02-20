7bc8ea394f7ef59d1a3a4ad70f960bd71ab43d25
Jens Axboe <axboe@kernel.dk>
2022-02-20 11:36:35 -0700

    io_uring.h: add IORING_SETUP_SQE128
    This asks the kernel to setup a ring with 128-byte SQE entries. May fail
    with -EINVAL if the kernel doesn't support this feature. If the kernel
    does support the feature, then the ring will support big sqe entries
    which some commands may require.
    
    Signed-off-by: Jens Axboe <axboe@kernel.dk>

--- src/include/liburing/io_uring.h
+++ src/include/liburing/io_uring.h
@@ -113,6 +113,7 @@ enum {
 #define IORING_SETUP_CLAMP	(1U << 4)	/* clamp SQ/CQ ring sizes */
 #define IORING_SETUP_ATTACH_WQ	(1U << 5)	/* attach to existing wq */
 #define IORING_SETUP_R_DISABLED	(1U << 6)	/* start with ring disabled */
+#define IORING_SETUP_SQE128	(1U << 7)	/* SQEs are 128b */
 
 enum {
 	IORING_OP_NOP,
